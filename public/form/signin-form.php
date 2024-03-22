9c8931adc19f40026ec78f02cc1b9b0e053ee230
xray2kp <151404726+xray2kp@users.noreply.github.com>
2024-03-22 12:23:21 -0400

    mega stuff

--- public/form/signin-form.php
+++ public/form/signin-form.php
@@ -3,15 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../assets/css/styles.css">
+	<link rel="stylesheet" href="/PHPGames-main/public/assets/css/style.css">
 	<title>Sign In</title>
-	<link rel="stylesheet" href="/">
-	
 
 </head>
 <body>
 
-	<div id="errorMessages"></div> <!-- Place for error messages -->
+	<div id="errorMessages"></div> 
 	
 	<form id="signinForm" method="post">
 		<h2>Sign In</h2>
